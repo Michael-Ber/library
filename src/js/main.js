34cae2512aadd9ee6d2bf420c9d585dc59833035
Michael <example@gmail.com>
2021-01-05 16:17:06 +0800

    dropdown menu

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,3 @@ $('.btn-warning').click(function() {
     $('.text1, .text2').fadeToggle('4000');
 });
 
-function test() {
-    console.log('1');
-    const _test1 = () => {
-        return 1+1;
-    };
-    return _test1();
-}
-
-const a = test();
-console.log(a);
